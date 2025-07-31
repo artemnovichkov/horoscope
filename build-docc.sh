442f6f443aa390874e7cf14efcc79fe3c49b0fda
Artem Novichkov <mail@artemnovichkov.com>
2025-07-31 20:33:17 +0500

    Add docs

---
+++ build-docc.sh
@@ -0,0 +1,13 @@
+##!/bin/sh
+
+xcrun xcodebuild docbuild \
+    -scheme Horoscope \
+    -destination 'generic/platform=iOS Simulator' \
+    -derivedDataPath "$PWD/.derivedData"
+
+xcrun docc process-archive transform-for-static-hosting \
+    "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/Horoscope.doccarchive" \
+    --output-path ".docs" \
+    --hosting-base-path "horoscope"
+
+echo '<script>window.location.href += "/documentation/horoscope"</script>' > .docs/index.html
