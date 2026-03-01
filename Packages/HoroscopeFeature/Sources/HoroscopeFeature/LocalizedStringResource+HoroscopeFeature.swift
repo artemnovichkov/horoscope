251b0d25008c0ec17d4c3a9f9ca7dbf3cf427681
Artem Novichkov <mail@artemnovichkov.com>
2026-03-01 18:15:41 +0500

    Add localization to HoroscopeFeature

---
+++ Packages/HoroscopeFeature/Sources/HoroscopeFeature/LocalizedStringResource+HoroscopeFeature.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+extension LocalizedStringResource {
+    static var yourHoroscopeSign: LocalizedStringResource {
+        LocalizedStringResource("Your Horoscope Sign:", bundle: .module)
+    }
+}
