1f2ca39aaf37260522b32c266d9bff59b3fbd17c
Artem Novichkov <mail@artemnovichkov.com>
2026-03-01 17:57:08 +0500

    Remove HoroscopeService, inline logic into HoroscopeClient.live

--- Packages/HoroscopeClient/Sources/HoroscopeClientLive/HoroscopeClientLive.swift
+++ Packages/HoroscopeClient/Sources/HoroscopeClientLive/HoroscopeClientLive.swift
@@ -1,21 +1,45 @@
+import FoundationModels
+import GithubClientLive
 import HoroscopeClient
 
 @MainActor
 public extension HoroscopeClient {
     static let live: HoroscopeClient = {
-        let service = HoroscopeService()
+        let session = LanguageModelSession(tools: [UserInfoTool(), GithubInfoTool()]) {
+            """
+            Your job is to create a horoscope for developers.
+
+            For each request from the user:
+            - get the zodiac sign and the gender using the fetchUserInfo tool.
+            - get Github data using the fetchGithubInfo tool.
+            """
+        }
+
+        @PromptBuilder
+        func prompt(username: String) -> Prompt {
+            "Generate a funny, witty today horoscope combining zodiac sign, gender and coding activity for username: \(username)."
+        }
+
         return HoroscopeClient(
             generate: { username in
-                service.horoscopeStream(username: username)
+                session.streamResponse(generating: Horoscope.self) {
+                    prompt(username: username)
+                }
             },
             horoscope: { username in
-                try await service.horoscope(username: username)
+                try await session.respond(generating: Horoscope.self) {
+                    prompt(username: username)
+                }.content
             },
             prewarm: { username in
-                service.prewarm(username: username)
+                var promptPrefix: Prompt?
+                if let username {
+                    promptPrefix = prompt(username: username)
+                }
+                session.prewarm(promptPrefix: promptPrefix)
             },
             session: {
-                service.session
+                session
             }
         )
     }()
